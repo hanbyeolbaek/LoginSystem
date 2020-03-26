62187e2a62f81d0f7eab5adfa6f34599d09854b0
hbbaek <hb.baek@gmail.com>
2020-03-26 15:09:22 +0900

    Oracle member 테이블 SQL문

---
+++ login_sys.sql
@@ -0,0 +1,12 @@
+drop table member;
+
+create table member(
+num number(20) PRIMARY key not null,
+email varchar2(50) not null,
+name varchar2(28) not null,
+pwd varchar2(30) not null,
+time TIMESTAMP not null);
+
+insert into member VALUES (0, 'admin', '°ü¸®ÀÚ', 'admin', systimestamp);
+
+select * from member;
